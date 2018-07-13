143baaf5af4d41dcc02417d6fae77b338ae4760b
YUUKI  KAWANO <yuki.rx0126@gmail.com>
2018-07-13 14:21:49 +0900

    test

---
+++ Kyoto_VR_OnWinMR/Assets/Scripts/CameraControl.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraControl : MonoBehaviour
+{
+
+    GameObject Player;
+    GameObject mainCamera;
+    void Start()
+    {
+        Player = GameObject.Find("Player");
+        mainCamera = GameObject.Find("Main Camera");
+    }
+
+    void Update()
+    {
+
+        mainCamera.transform.position = new Vector3(Player.transform.position.x - 100, Player.transform.position.y, Player.transform.position.z );
+
+    }
+}
---
+++ Kyoto_VR_OnWinMR/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 10.0f;
+    float x = 0f;
+    float z = 0f;
+
+    CharacterController controller;
+
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        x = Input.GetAxis("Horizontal") * speed;
+        z = Input.GetAxis("Vertical") * speed;
+        Vector3 direction = new Vector3(x, 0, z);
+
+        controller.SimpleMove(direction);
+    }
+}
