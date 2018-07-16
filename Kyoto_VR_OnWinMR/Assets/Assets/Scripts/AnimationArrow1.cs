4545c4d287173e8bc1a4243f97887c0b8a85984f
YUUKI  KAWANO <yuki.rx0126@gmail.com>
2018-07-16 13:34:13 +0900

    追加
    矢印をふやして、iPhoneのおおきさ、位置を変えました

---
+++ Kyoto_VR_OnWinMR/Assets/Assets/Scripts/AnimationArrow1.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationArrow1 : MonoBehaviour {
+    Animator animator;
+    bool ArrowTrigger = false;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
