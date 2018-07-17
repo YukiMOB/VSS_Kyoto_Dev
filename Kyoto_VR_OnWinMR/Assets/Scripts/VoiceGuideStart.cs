abd5d194b5698198ca234a6be789155e604326b0
YUUKI  KAWANO <yuki.rx0126@gmail.com>
2018-07-17 17:22:22 +0900

    音声案内（仮）実装

---
+++ Kyoto_VR_OnWinMR/Assets/Scripts/VoiceGuideStart.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VoiceGuideStart : MonoBehaviour {
+    public AudioSource AS;
+    public GameObject obj;
+    public AudioClip AC;
+    bool startFlag;
+
+    void Start()
+    {
+        //AudioSourceコンポーネントを取得し、変数に格納
+        AS = gameObject.GetComponent<AudioSource>();
+        startFlag = true;
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if(other.gameObject.tag == obj.tag && startFlag)
+        {
+            AS.PlayOneShot(AC);
+            startFlag = false;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        startFlag = true;
+    }
+}
